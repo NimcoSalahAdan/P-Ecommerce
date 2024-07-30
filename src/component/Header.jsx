eb3eebca6fb54b51290faf2c308ebf11005918f5
NimcoSalahAdan <bestiesalah916@gmail.com>
2024-07-30 10:44:48 +0300

    second initialize

---
+++ src/component/Header.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <header className="pb-6 bg-white lg:pb-0">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <nav className="flex items-center justify-between h-16 lg:h-20">
+            <div className="">
+              <h1 className="text-4xl">Ecommerce App</h1>
+            </div>
+
+            <button
+              type="button"
+              className="inline-flex p-2 text-black transition-all duration-200 rounded-md lg:hidden focus:bg-gray-100 hover:bg-gray-100"
+            >
+              <svg
+                className="block w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h16M4 16h16" />
+              </svg>
+
+              <svg
+                className="hidden w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+
+            <div className="hidden lg:flex lg:items-center lg:ml-auto lg:space-x-10">
+              <Link to="/" className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                Home
+              </Link>
+
+              <Link to="/About" className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                About
+              </Link>
+
+              <Link to="/Men" className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                Men
+              </Link>
+
+              <Link to="/Women" className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                Women
+              </Link>
+              <Link to="/Contact" className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                Contact
+              </Link>
+            </div>
+
+            <a
+              href="#"
+              className="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700"
+              role="button"
+            >
+              Get started now
+            </a>
+          </nav>
+
+          <nav className="pt-4 pb-6 bg-white border border-gray-200 rounded-md shadow-md lg:hidden">
+            <div className="flow-root">
+              <div className="flex flex-col px-6 -my-2 space-y-1">
+                <a href="#" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                  Features
+                </a>
+
+                <a href="#" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                  Solutions
+                </a>
+
+                <a href="#" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                  Resources
+                </a>
+
+                <a href="#" className="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+                  Pricing
+                </a>
+              </div>
+            </div>
+
+            <div className="px-6 mt-6">
+              <a
+                href="#"
+                className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md items-center hover:bg-blue-700 focus:bg-blue-700"
+                role="button"
+              >
+                
+              </a>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
