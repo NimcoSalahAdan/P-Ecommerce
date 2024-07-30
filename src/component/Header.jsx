dba836ee91500b2c0454f5edae4cf7fdee1cde8d
NimcoSalahAdan <bestiesalah916@gmail.com>
2024-07-30 14:00:07 +0300

    ts

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -57,13 +57,7 @@ function Header() {
               </Link>
             </div>
 
-            <a
-              href="#"
-              className="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700"
-              role="button"
-            >
-              Get started now
-            </a>
+          
           </nav>
 
           <nav className="pt-4 pb-6 bg-white border border-gray-200 rounded-md shadow-md lg:hidden">
